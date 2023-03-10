49b8a615469759ed049d52297686f1ad3bf09a21
mexus <spikemorow@gmail.com>
2023-03-11 01:24:40 +0300

    module4 task done

--- js/main.js
+++ js/main.js
@@ -15,3 +15,16 @@ function checkMaxLength (str, maxLength) {
   str.length <= maxLength;
 }
 
+const photos = [];
+
+for (let i = 1; i < 26; i++) {
+  const photo = {
+    id: i,
+    url: `photos/${i}.jpg`,
+    description: `Фото под номером ${i}!`,
+    likes: randomNumber(15, 200),
+    comments: randomNumber(0, 200)
+  };
+  photos.push(photo);
+}
+
