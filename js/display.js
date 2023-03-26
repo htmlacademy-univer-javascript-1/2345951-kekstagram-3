17c8063dd13b7d3a559fd4f25068a35d15bd2016
mexus <spikemorow@gmail.com>
2023-03-26 20:35:36 +0300

    display fixes

--- js/display.js
+++ js/display.js
@@ -1,20 +1,19 @@
 import { photos } from './data.js';
 
-const template = document.querySelectorAll('#picture').content;
+const pictureTemplate = document.querySelector('#picture').content;
 const fragment = document.createDocumentFragment();
 const pictures = document.querySelector('.pictures');
 
 
-for (let i = 0; i <= photos.length; i++) {
-  const picture = photos[i];
-  const newElement = template.cloneNode(true);
+photos.forEach((photo) => {
+  const newElement = pictureTemplate.cloneNode(true);
 
-  newElement.querySelector('.picture__img').src = picture.url;
-  newElement.querySelector('.picture__comments').textContent = picture.comments;
-  newElement.querySelector('.picture__likes').textContent = picture.likes;
+  newElement.querySelector('.picture__img').src = photo.url;
+  newElement.querySelector('.picture__comments').textContent = photo.comments;
+  newElement.querySelector('.picture__likes').textContent = photo.likes;
 
   fragment.appendChild(newElement);
-}
+});
 
 pictures.appendChild(fragment);
 
