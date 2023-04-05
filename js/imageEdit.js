f2aefdcc3205d17149954bbdb631e6d89043d156
mexus <spikemorow@gmail.com>
2023-04-05 22:38:40 +0300

    effects done

--- js/imageEdit.js
+++ js/imageEdit.js
@@ -5,6 +5,41 @@ const decreaseButton = document.querySelector('.scale__control--smaller');
 const imgUploadPreview = document.querySelector('.img-upload__preview');
 imgUploadPreview.style.setProperty('transform','scale(1.0)');
 let styleValueNumber = imgUploadPreview.style.getPropertyValue('transform').replace(/\D/g,'');
+const chromeLable = document.querySelector('#effect-chrome');
+const originalLable = document.querySelector('#effect-none');
+const sepiaLabel = document.querySelector('#effect-sepia');
+const marvinLabel = document.querySelector('#effect-marvin');
+const phobosLabel = document.querySelector('#effect-phobos');
+const heatLabel = document.querySelector('#effect-heat');
+
+heatLabel.addEventListener('click', () => {
+  setEffect('heat');
+});
+
+phobosLabel.addEventListener('click', () => {
+  setEffect('phobos');
+});
+
+marvinLabel.addEventListener('click', () => {
+  setEffect('marvin');
+});
+
+sepiaLabel.addEventListener('click', () => {
+  setEffect('sepia');
+});
+
+chromeLable.addEventListener('click', () => {
+  setEffect('chrome');
+});
+
+originalLable.addEventListener('click', () => {
+  imgUploadPreview.querySelector('img').className = '';
+});
+
+function setEffect(effect) {
+  imgUploadPreview.querySelector('img').className = '';
+  imgUploadPreview.querySelector('img').classList.add(`effects__preview--${effect}`);
+}
 
 function increaseScale() {
   if (scaleValueNumber === 100) {
