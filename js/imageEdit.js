ae0a18d735eee11d821a1aeb83fb42d811ebe8d8
mexus <spikemorow@gmail.com>
2023-04-06 01:14:56 +0300

    slider done

--- js/imageEdit.js
+++ js/imageEdit.js
@@ -11,6 +11,95 @@ const sepiaLabel = document.querySelector('#effect-sepia');
 const marvinLabel = document.querySelector('#effect-marvin');
 const phobosLabel = document.querySelector('#effect-phobos');
 const heatLabel = document.querySelector('#effect-heat');
+const sliderElement = document.querySelector('.effect-level__slider');
+const sliderElementValue = document.querySelector('.effect-level__value');
+let sliderValue;
+let currentEffect = 'original';
+const filters = {
+  'chrome': {
+    min: 0,
+    max: 1,
+    name: 'grayscale',
+    step: 0.1,
+    measure: ''
+  },
+  'sepia' : {
+    min: 0,
+    max: 1,
+    name: 'sepia',
+    step: 0.1,
+    measure: ''
+  },
+  'marvin': {
+    min: 0,
+    max: 100,
+    name: 'invert',
+    step: 1,
+    measure: '%'
+  },
+  'phobos': {
+    min: 0,
+    max: 3,
+    name: 'blur',
+    step: 0.1,
+    measure: 'px'
+  },
+  'heat': {
+    min: 1,
+    max: 3,
+    name: 'brightness',
+    step: 0.1,
+    measure: ''
+  }
+};
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
+});
+sliderElement.setAttribute('disabled', true);
+
+
+function setEffect(effect, flag) {
+  const image = imgUploadPreview.querySelector('img');
+  if (effect === 'original') {
+    image.className = '';
+    return;
+  }
+  const minValue = filters[effect].min;
+  const maxValue = filters[effect].max;
+  const stepValue = filters[effect].step;
+  const measureValue = filters[effect].measure;
+  const nameValue = filters[effect].name;
+  if (!flag) {
+    sliderElement.noUiSlider.updateOptions({
+      range: {
+        min: minValue,
+        max: maxValue,
+      },
+      start: maxValue,
+      step: stepValue
+    });
+    sliderElement.removeAttribute('disabled', true);
+    image.className = '';
+    image.classList.add(`effects__preview--${effect}`);
+    currentEffect = effect;
+  }
+  else {
+    image.style.filter = `${nameValue}(${sliderValue}${measureValue})`;
+  }
+}
+
+sliderElement.noUiSlider.on('update', () => {
+  sliderElementValue.value = sliderElement.noUiSlider.get();
+  sliderValue = sliderElement.noUiSlider.get();
+  setEffect(currentEffect, true);
+});
 
 heatLabel.addEventListener('click', () => {
   setEffect('heat');
@@ -33,13 +122,8 @@ chromeLable.addEventListener('click', () => {
 });
 
 originalLable.addEventListener('click', () => {
-  imgUploadPreview.querySelector('img').className = '';
+  setEffect('original');
 });
-
-function setEffect(effect) {
-  imgUploadPreview.querySelector('img').className = '';
-  imgUploadPreview.querySelector('img').classList.add(`effects__preview--${effect}`);
-}
 
 function increaseScale() {
   if (scaleValueNumber === 100) {
